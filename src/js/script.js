8a93e46dbee4b973cd0fc88bc70f44a49fa4ca2b
Nikolay Litvinenko <Litvinenko_n.v@mail.ru>
2023-02-24 12:46:30 +0300

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,155 +1,155 @@
 const accaunt1 = {
   useName: "Николай",
-  iphone: "+79964179001",
+  iphone: +79964179001,
   transaction: [800, -150, 1500, -500, -170, -37, 5000, -1470, -800, 450],
-  interestBalans: 1.5,
+  interestBalans: 1.8,
   password: 1111,
   transactionDate: [
-    "Tue Feb 21 2023 16:32:45",
-    "Tue Feb 20 2023 11:13:45",
-    "Tue Feb 19 2023 17:33:45",
-    "Tue Feb 18 2023 10:12:45",
-    "Tue Feb 17 2023 13:52:45",
-    "Tue Feb 16 2023 18:02:45",
-    "Tue Feb 16 2023 23:32:45",
-    "Tue Feb 15 2023 17:32:45",
     "Tue Feb 14 2023 09:55:45",
     "Tue Feb 14 2023 14:16:45",
+    "Tue Feb 15 2023 17:32:45",
+    "Tue Feb 16 2023 18:02:45",
+    "Tue Feb 16 2023 23:32:45",
+    "Tue Feb 17 2023 13:52:45",
+    "Tue Feb 18 2023 10:12:45",
+    "Tue Feb 19 2023 17:33:45",
+    "Tue Feb 20 2023 11:13:45",
+    "Tue Feb 21 2023 16:32:45",
   ],
   currency: "RUB",
   locale: "ru-RU",
 };
 const accaunt2 = {
   useName: "Татьяна",
-  iphone: "+79886298315",
+  iphone: +79886298315,
   transaction: [5500, -150, 15000, -800, -100, -137, 5000, -1770, -620, 40070],
   interestBalans: 1.7,
   password: 2222,
   transactionDate: [
-    "Tue Feb 21 2023 16:32:45",
+    "Tue Feb 1 2023 14:16:45",
+    "Tue Feb 4 2023 09:55:45",
+    "Tue Feb 7 2023 10:12:45",
+    "Tue Feb 7 2023 17:32:45",
+    "Tue Feb 10 2023 23:32:45",
+    "Tue Feb 16 2023 18:02:45",
+    "Tue Feb 17 2023 13:52:45",
     "Tue Feb 19 2023 11:13:45",
     "Tue Feb 19 2023 17:33:45",
-    "Tue Feb 7 2023 10:12:45",
-    "Tue Feb 17 2023 13:52:45",
-    "Tue Feb 16 2023 18:02:45",
-    "Tue Feb 10 2023 23:32:45",
-    "Tue Feb 7 2023 17:32:45",
-    "Tue Feb 4 2023 09:55:45",
-    "Tue Feb 1 2023 14:16:45",
+    "Tue Feb 21 2023 16:32:45",
   ],
   currency: "RUB",
   locale: "ru-RU",
 };
 const accaunt3 = {
   useName: "Денис",
-  iphone: "+79064111997",
+  iphone: +79064111997,
   transaction: [1500, -1050, 1740, -420, -100, 1370, 3500, -1770, -620, 470],
   interestBalans: 1.2,
   password: 3333,
   transactionDate: [
-    "Tue Feb 21 2023 16:32:45",
-    "Tue Feb 20 2023 11:13:45",
-    "Tue Feb 19 2023 17:33:45",
-    "Tue Feb 18 2023 10:12:45",
-    "Tue Feb 17 2023 13:52:45",
-    "Tue Feb 16 2023 18:02:45",
-    "Tue Feb 16 2023 23:32:45",
-    "Tue Feb 15 2023 17:32:45",
     "Tue Feb 14 2023 09:55:45",
     "Tue Feb 14 2023 14:16:45",
+    "Tue Feb 15 2023 17:32:45",
+    "Tue Feb 16 2023 18:02:45",
+    "Tue Feb 16 2023 23:32:45",
+    "Tue Feb 17 2023 13:52:45",
+    "Tue Feb 18 2023 10:12:45",
+    "Tue Feb 19 2023 17:33:45",
+    "Tue Feb 20 2023 11:13:45",
+    "Tue Feb 21 2023 16:32:45",
   ],
   currency: "RUB",
   locale: "ru-RU",
 };
 const accaunt4 = {
   useName: "Юленэ",
-  iphone: "+79624254996",
+  iphone: +79624254996,
   transaction: [1200, -360, 1600, -120, -100, -137, 2150, -1770, -620, 470],
   interestBalans: 1.4,
   password: 4444,
   transactionDate: [
-    "Tue Feb 21 2023 16:32:45",
-    "Tue Feb 20 2023 11:13:45",
-    "Tue Feb 19 2023 17:33:45",
-    "Tue Feb 18 2023 10:12:45",
+    "Tue Feb 14 2023 09:55:45",
+    "Tue Feb 14 2023 14:16:45",
+    "Tue Feb 15 2023 17:32:45",
     "Tue Feb 17 2023 13:52:45",
     "Tue Feb 16 2023 18:02:45",
     "Tue Feb 16 2023 23:32:45",
-    "Tue Feb 15 2023 17:32:45",
-    "Tue Feb 14 2023 09:55:45",
-    "Tue Feb 14 2023 14:16:45",
+    "Tue Feb 18 2023 10:12:45",
+    "Tue Feb 19 2023 17:33:45",
+    "Tue Feb 20 2023 11:13:45",
+    "Tue Feb 21 2023 16:32:45",
   ],
   currency: "RUB",
   locale: "ru-RU",
 };
 const accaunt5 = {
   useName: "Ирина",
-  iphone: "+79880886141",
+  iphone: +79880886141,
   transaction: [
     -5000, -360, 16000, -1200, -100, -1370, 2150, -1770, -620, 4070,
   ],
   interestBalans: 1.1,
   password: 5555,
   transactionDate: [
-    "Tue Feb 21 2023 16:32:45",
-    "Tue Feb 20 2023 11:13:45",
-    "Tue Feb 19 2023 17:33:45",
-    "Tue Feb 18 2023 10:12:45",
-    "Tue Feb 17 2023 13:52:45",
-    "Tue Feb 16 2023 18:02:45",
-    "Tue Feb 16 2023 23:32:45",
-    "Tue Feb 15 2023 17:32:45",
     "Tue Feb 14 2023 09:55:45",
     "Tue Feb 14 2023 14:16:45",
+    "Tue Feb 15 2023 17:32:45",
+    "Tue Feb 16 2023 18:02:45",
+    "Tue Feb 16 2023 23:32:45",
+    "Tue Feb 17 2023 13:52:45",
+    "Tue Feb 18 2023 10:12:45",
+    "Tue Feb 19 2023 17:33:45",
+    "Tue Feb 20 2023 11:13:45",
+    "Tue Feb 21 2023 16:32:45",
   ],
   currency: "RUB",
   locale: "ru-RU",
 };
 const accaunt6 = {
   useName: "Иван",
-  iphone: "+79288224690",
+  iphone: +79288224690,
   transaction: [120, -3060, 1600, -120, -1000, -137, 2550, -1770, -620, 4070],
   interestBalans: 1.9,
   password: 6666,
   transactionDate: [
-    "Tue Feb 21 2023 16:32:45",
-    "Tue Feb 20 2023 11:13:45",
-    "Tue Feb 19 2023 17:33:45",
-    "Tue Feb 18 2023 10:12:45",
-    "Tue Feb 17 2023 13:52:45",
-    "Tue Feb 16 2023 18:02:45",
-    "Tue Feb 16 2023 23:32:45",
-    "Tue Feb 15 2023 17:32:45",
     "Tue Feb 14 2023 09:55:45",
     "Tue Feb 14 2023 14:16:45",
+    "Tue Feb 15 2023 17:32:45",
+    "Tue Feb 16 2023 18:02:45",
+    "Tue Feb 16 2023 23:32:45",
+    "Tue Feb 17 2023 13:52:45",
+    "Tue Feb 18 2023 10:12:45",
+    "Tue Feb 19 2023 17:33:45",
+    "Tue Feb 20 2023 11:13:45",
+    "Tue Feb 21 2023 16:32:45",
   ],
   currency: "RUB",
   locale: "ru-RU",
 };
 const accaunt7 = {
   useName: "Антонио",
-  iphone: "+79624333191",
+  iphone: +79624333191,
   transaction: [
     12000, -3060, -1600, -180, -1000, -137, 2150, -1770, -620, -470,
   ],
   interestBalans: 1.3,
   password: 7777,
   transactionDate: [
-    "Tue Feb 21 2023 16:32:45",
-    "Tue Feb 20 2023 11:13:45",
-    "Tue Feb 18 2023 17:33:45",
-    "Tue Feb 18 2023 10:12:45",
-    "Tue Feb 16 2023 13:52:45",
+    "Tue Feb 2 2023 14:16:45",
+    "Tue Feb 9 2023 09:55:45",
+    "Tue Feb 15 2023 17:32:45",
     "Tue Feb 16 2023 18:02:45",
     "Tue Feb 16 2023 23:32:45",
-    "Tue Feb 15 2023 17:32:45",
-    "Tue Feb 9 2023 09:55:45",
-    "Tue Feb 2 2023 14:16:45",
+    "Tue Feb 16 2023 13:52:45",
+    "Tue Feb 18 2023 10:12:45",
+    "Tue Feb 18 2023 17:33:45",
+    "Tue Feb 20 2023 11:13:45",
+    "Tue Feb 21 2023 16:32:45",
   ],
   currency: "RUB",
   locale: "ru-RU",
 };
-const arrayAccount = [
+const accounts = [
   accaunt1,
   accaunt2,
   accaunt3,
@@ -161,86 +161,129 @@ const arrayAccount = [
 
 ///////////////////////////////////////////////////////
 //Блоки
-let displayTrans = document.querySelector(".display-transaction");
+let displayTransaction = document.querySelector(".display-transaction");
+let commonWrapper = document.querySelector(".wrapper-content");
 //Кнопки
-
+let btnLoginBank = document.querySelector(".header__btn-login");
+let btnCloseBank = document.querySelector(".header__btn-login-close");
 //Формы
-
+let inputNicknameLogin = document.querySelector(".header__nike-name");
+let inputPasswordLogin = document.querySelector(".header__password");
 //Строки
-let deposiText = document.querySelector(".receiving");
-let spentText = document.querySelector(".spent");
-let percentText = document.querySelector(".percent");
-let balansText = document.querySelector(".account-details__current-balance");
+let labelDeposi = document.querySelector(".receiving");
+let labelSpent = document.querySelector(".spent");
+let labelPercent = document.querySelector(".percent");
+let currentBalanse = document.querySelector(
+  ".account-details__current-balance"
+);
 let currentDate = document.querySelector(".account-details__current-date");
+let dateOperation = document.querySelector(
+  ".display-transaction__date-operation"
+);
 //////////////////////////////////////////////////////
 const getCurrency = function (currency, locale) {
-  let y = new Intl.NumberFormat(locale, { style: "currency", currency })
+  let curr = new Intl.NumberFormat(locale, { style: "currency", currency })
     .formatToParts(1)
     .find((x) => x.type === "currency").value;
-
-  return y;
+  return curr;
 };
 
-const displayTransaction = function (acc) {
-  displayTrans.innerHTML = "";
-  let transaction = acc.transaction;
+const getTransaction = function(accaunt) {
+  displayTransaction.innerHTML = ''
+  let transaction = accaunt.transaction
 
   transaction.forEach(function (trans, index) {
-    let operation = trans < 0 ? "write-offs" : "deposit";
-    let whatOperation = trans < 0 ? " Списание" : "Пополнение";
-    let currency = getCurrency(acc.currency);
-   
-    let wasDate = new Date(acc.transactionDate[index]);
+    const operationClass = trans > 0 ? "deposit" : "write-offs";
+    const operationText = trans > 0 ? "Пополнение" : "Списание";
+
+    let currency = getCurrency(accaunt.currency);
+    let wasDate = new Date(accaunt.transactionDate[index]);
     let date = `${wasDate.getDate()}`.padStart(2, "0");
     let month = `${wasDate.getMonth() + 1}`.padStart(2, "0");
     let year = wasDate.getFullYear();
-
     let transData = `${date}.${month}.${year}`;
+    
+    const transactionRow = `
+       <div class="display-transaction__row">
+          <h2 class="display-transaction__${operationClass}">${operationText}</h2>
+          <h2 class="display-transaction__date-operation">${transData}</h2>
+          <h2 class="display-transaction__amount">${trans} ${currency}</h2>
+        </div>
+    `;
 
-    console.log();
-    let transactionRow = `
-      <div class="display-transaction__row">
-         <h2 class="display-transaction__${operation}">${whatOperation}</h2>
-         <h2 class="display-transaction__date-operation">${transData}</h2>
-         <h2 class="display-transaction__amount">${trans} ${currency}</h2>
-      </div>
-      `;
+    displayTransaction.insertAdjacentHTML('afterbegin', transactionRow)
+  } );
+}
+// getTransaction(accaunt7)
 
-    displayTrans.insertAdjacentHTML("beforeend", transactionRow);
-  });
-};
-displayTransaction(accaunt7);
 
-const getFinalBalance = function (trans) {
-  let currency = getCurrency(trans.currency);
-  let currentBalance = trans.transaction.reduce((acc, item) => acc + item, 0);
-  let getDeposits = trans.transaction.reduce((acc, item) => {
+const getFinalBalance = function(accaunt) {
+  let currency = getCurrency(accaunt.currency);
+  let balance = accaunt.transaction.reduce((acc, item) => acc + item)
+  let deposit = accaunt.transaction.reduce((acc, item) => {
     if (item >= 0) {
       return acc + item;
     } else {
       return acc;
     }
   }, 0);
-  let getWriteOff = trans.transaction.reduce((acc, item) => {
+  let writeOff = accaunt.transaction.reduce((acc, item) => {
     if (item <= 0) {
       return acc + item;
     } else {
       return acc;
     }
   }, 0);
-  let getPercent = currentBalance * (trans.interestBalans / 100);
+  let percent = balance * (accaunt.interestBalans / 100);
 
-  balansText.textContent = `${currentBalance} ${currency}`;
-  deposiText.textContent = `${getDeposits} ${currency}`;
-  spentText.textContent = `${getWriteOff} ${currency}`;
-  percentText.textContent = `${Math.floor(getPercent)} ${currency}`;
-};
-getFinalBalance(accaunt7);
+  currentBalanse.textContent = `${balance} ${currency}`
+  labelDeposi.textContent = `${deposit} ${currency}`
+  labelSpent.textContent = `${writeOff} ${currency}`
+  labelPercent.textContent = `${Math.floor(percent)} ${currency}`
 
-const getTransactionDate = function (transDate) {
+}
+
+// getFinalBalance(accaunt7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let currentAccaunt;
+
+btnLoginBank.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  let nickname = Number(inputNicknameLogin.value);
+  let password = Number(inputPasswordLogin.value);
   let now = new Date();
-  let nowData = new Intl.DateTimeFormat(transDate.locale).format(now);
-  currentDate.textContent = `На ${nowData}`;
-};
+  let date = `${now.getDate()}`.padStart(2, "0");
+  let month = `${now.getMonth() + 1}`.padStart(2, "0");
+  let year = `${now.getFullYear()}`;
+  
+  let nowDate = `${date}.${month}.${year}`
 
-getTransactionDate(accaunt7);
+  currentAccaunt = accounts.find(accaunt => accaunt.iphone === nickname)
+  if(currentAccaunt?.password === password) {
+    inputNicknameLogin.value = "";
+    inputPasswordLogin.value = "";
+    commonWrapper.style.visibility = "visible";
+    currentDate.textContent = nowDate
+  }
+
+  getTransaction(currentAccaunt);
+  getFinalBalance(currentAccaunt);
+  
+})
+
+
